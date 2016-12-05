4b164c77eab38a76d43585bad446fa48f74c82c9
Anthony O <netangel+github@gmail.com>
2016-12-05 19:08:20 +0100

    Correctly stop H2 server.

--- src/main/java/com/github/anthony_o/swingifts/servlet/SwingiftsServletContextListener.java
+++ src/main/java/com/github/anthony_o/swingifts/servlet/SwingiftsServletContextListener.java
@@ -1,6 +1,9 @@
 package com.github.anthony_o.swingifts.servlet;
 
 import com.github.anthony_o.swingifts.util.InjectUtils;
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -10,20 +13,22 @@ import java.sql.SQLException;
 
 @WebListener
 public class SwingiftsServletContextListener implements ServletContextListener {
+    private final static Logger LOG = LoggerFactory.getLogger(SwingiftsServletContextListener.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("Swingifts started");
+        LOG.info("Swingifts started");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        System.out.println("Swingifts stoping");
-        //InjectUtils.getInstance(Server.class).shutdown();
+        LOG.info("Swingifts stoping");
+        InjectUtils.getInstance(Server.class).stop();
         try {
             InjectUtils.getInstance(Connection.class).createStatement().execute("SHUTDOWN"); // close all the H2 connections thanks to http://stackoverflow.com/a/40762283/535203
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.error("Problem while shutting down H2", e);
         }
-        System.out.println("Swingifts stopped");
+        LOG.info("Swingifts stopped");
     }
 }
